93834a2f2de05455637d76ae4823dd90ff5eb1d8
Lai <laitin_man@hotmail.comm>
2019-01-09 15:37:45 +0100

    new add

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,8 +1,14 @@
 function submitComment() {
+    const inputField = document.getElementById('name')
+    const name = inputField.value
+
+    const textArea = document.getElementById('msg')
+    const msg = textArea.value
+
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
-  
+    
     h3.innerHTML = `${name} said:`
     p.innerHTML = msg
     comment.classList.add('comment')
@@ -10,4 +16,4 @@ function submitComment() {
     comment.appendChild(p)
   
     console.log(comment)
-  }
+}
